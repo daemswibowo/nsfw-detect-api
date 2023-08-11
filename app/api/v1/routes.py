b1e8805e3c3a8fbbab4c917405800792346f25ad
Dimas Wibowo <daemswibowo@gmail.com>
2023-08-11 11:31:02 +0700

    remove todos example

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from .controllers.todo_controller import router as todo_router
 from .controllers.nsfw_controller import router as nsfw_router
 
 routes = APIRouter(prefix="/v1")
-_routers = [todo_router, nsfw_router]
+_routers = [nsfw_router]
 
 for router in _routers:
     routes.include_router(router)
--- app/commons/di.py
+++ app/commons/di.py
@@ -3,16 +3,13 @@ from dependency_injector import containers, providers
 from app.data.sources.database import Database
 from app.data.sources.nsfw_detector import NsfwDetector
 from app.commons.config import settings
-from app.repositories.todo_repository import TodoRepository
 from app.repositories.nsfw_repository import NsfwRepository
-from app.api.v1.services.todo_service import TodoService
 from app.api.v1.services.nsfw_service import NsfwService
 
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
         modules=[
-            "app.api.v1.controllers.todo_controller",
             "app.api.v1.controllers.nsfw_controller",
         ]
     )
@@ -22,9 +19,7 @@ class Container(containers.DeclarativeContainer):
     nsfw_detector = providers.Singleton(NsfwDetector)
 
     # repositories
-    todo_repository = providers.Factory(TodoRepository, db=db.provided.session)
     nsfw_repository = providers.Factory(NsfwRepository, nsfw_detector=nsfw_detector)
 
     # services
-    todo_service = providers.Factory(TodoService, repository=todo_repository)
     nsfw_service = providers.Factory(NsfwService, repository=nsfw_repository)
